9f6c3688a707e2ac62bab568342c47947d11583e
jawad-nsu <jawadurrahim1997@gmail.com>
2024-11-25 00:16:16 -0500

    retrigger deployment

--- pages/jobs/index.tsx
+++ pages/jobs/index.tsx
@@ -1,8 +1,3 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/KBH6Ik8LGqn
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import { AvatarImage, AvatarFallback, Avatar } from '@/components/ui/avatar';
 import {
   SelectValue,
